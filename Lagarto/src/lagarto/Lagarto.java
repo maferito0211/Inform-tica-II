93df973e735e85c1d6adb644945d1b6aa2c9cdd5
maferito0211 <61156810+maferito0211@users.noreply.github.com>
2020-02-19 15:55:03 -0500

    Add files via upload

---
+++ Lagarto/src/lagarto/Lagarto.java
@@ -0,0 +1,12 @@
+
+package lagarto;
+
+
+public class Lagarto {
+
+    public static void main(String[] args) {
+        FrmPiedra frmPiedra=new FrmPiedra();
+        frmPiedra.setVisible(true);
+    }
+    
+}
